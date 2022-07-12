92326a2825071a455a7af964ff49872d81f8f994
ccans <55026208+ccans@users.noreply.github.com>
2022-07-12 16:43:49 -0500

    basic changes

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,42 @@
-import React from 'react'
-import styles from '../styles/Header.module.css'
+import React, { useEffect, useState, useRef } from "react";
+import styles from '../styles/Header.module.css';
+
+import Link from 'next/link';
 
 function Header() {
 
-  
+  const copiedText = useRef(null);
 
+  function timeout(delay) {
+    return new Promise( res => setTimeout(res, delay) );
+  }
+
+  async function textCopied() {
+    copiedText.current.style.opacity = "1";
+    await timeout(1000);
+    copiedText.current.style.opacity = "0";
+  }
 
   return (
     <div className='header'> 
       <a className='inconsolata primary-txt'> Sai Shinde Art </a>
       <a className='roboto secondary-txt'> High school student based in Plymouth, Minnesota creating diverse art pieces across a variety of mediums  </a>
       <div className='button-box'>
-
+      <br /> <br />
+          <div className="button"> <Link href="/indextwo" >  Home </Link> </div>  <br /> 
+          <div className="button"><a href="#Paintings"> Paintings </a> </div> <br /> 
+          <div className="button"><a href="#Illustrations"> Illustrations </a> </div> <br /> 
+          <div className="button"> <a href="#Stickers"> Stickers </a> </div> <br /> 
+          <div className="button"><Link href="/about"> About Me </Link> </div> <br /> 
+          <div className="button"><Link href="/contact"> Contact </Link> </div> <br /> 
+      </div>
+      <div className='bottom-box'>
+        <div className='icons-box'> 
+          <a href="https://www.instagram.com/saispaintings_/"> <img className="icon" src="https://img.icons8.com/ios/50/808080/instagram-new--v1.png"/> </a>
+          <img className="iconTwo" src="https://img.icons8.com/ios-filled/50/808080/etsy.png"/>
+          <a onClick={() => {navigator.clipboard.writeText("saishinde.art"); textCopied()}}> <img className="icon" src="https://img.icons8.com/ios-glyphs/100/808080/share--v1.png"/> </a>
+          <a className="iconText" ref={copiedText} style={{opacity: "0"}}> copied! </a>
+        </div>
       </div>
     </div>
   )
--- components/Layout.js
+++ components/Layout.js
@@ -4,20 +4,16 @@ import Header from "./Header";
 import React, { useState, useRef, useEffect } from 'react';
 import { motion, AnimatePresence } from "framer-motion";
 import useLocalStorage from 'use-local-storage';
-
-import useDarkMode from 'use-dark-mode';
-
   
 
 export default function Layout({ children, router }) {
-    const darkMode = useDarkMode(false);
     
        
         return (
             <div>
                 
                 <div className="page-container">
-                    <Header mode={darkMode.value} darkMode={darkMode}> </Header>
+                    <Header> </Header>
                     <AnimatePresence exitBeforeEnter >
                         <motion.div className="content-container" key = {router.route} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ opacity: { duration: .5 }} }>
                             {children}
