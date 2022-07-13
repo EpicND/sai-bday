ef5864f5714cdea3841d9df817e62ef15b2dacf3
ccans <55026208+ccans@users.noreply.github.com>
2022-07-13 17:07:38 -0500

    fixes

--- components/Context.js
+++
@@ -1,8 +0,0 @@
-import { createContext, useContext, useEffect, useRef, useState } from "react";
-
-const MyContext = createContext();
-
-export const ContextStore = (props) => {
-  const ref = useRef();
-  return <MyContext.Provider ref={ref}>{props.children}</MyContext.Provider>;
-};
--- components/Layout.js
+++ components/Layout.js
@@ -7,35 +7,19 @@ import { motion, AnimatePresence } from "framer-motion";
 
 export default function Layout({ children, router }) {
 
-    const Context = createContext(null);
-
-    const ref = useRef(null);
-    const [ref_state, setRefState] = useState(null);
-
-    useEffect(() => {
-        if (!ref.current) {
-            return;
-        }
-
-        setRefState(ref.current)
-    }, []);
 
         return (
             <div > 
-                <Context.Provider value={ref.current}>
                 <div className="page-container" >
                     <Header route={router.route}> </Header>
                     <Dropdown route={router.route}> </Dropdown> 
                     <AnimatePresence exitBeforeEnter >
-                        <motion.div  onScroll={() => {
-                                    console.log(ref.current);
-                                }}
+                        <motion.div 
               className="content-container" key = {router.route} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ opacity: { duration: .5 }} }>
                             {children}
                         </motion.div>
                     </AnimatePresence>
                 </div>
-                </Context.Provider>
             </div>
         )
   }
